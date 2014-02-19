ef72325819c32a37b048649e96471a508e54d1b2
javanthemanly <2242255@javan-grahams-macbook.local>
2014-02-19 15:05:11 -0500

    hi

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,6 @@ public class Main {
 
     public static void main(String args[]){
 
-        boolean isDanielABeaner = true;
-
-        if(isDanielABeaner){
-            System.out.println("#Circumcision");
-        }else{
-            System.out.println("Beaner and Negroman moon landing!!!!!");
-        }
     }
 
 }
